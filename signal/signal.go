8b0e66604011b2b0944a4d5dbd119ee9802e0871
thesinglecloud <hanfeng@zdns.cn>
2019-02-18 03:50:22 +0000

    add signal handle into context

--- signal/signal.go
+++ signal/signal.go
@@ -1,17 +1,41 @@
 package signal
 
 import (
+	"context"
 	"os"
 	osig "os/signal"
 	"syscall"
 )
 
 func WaitForInterrupt(cb func()) {
-	signalCh := make(chan os.Signal)
-	osig.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
-	<-signalCh
+	ch := make(chan os.Signal)
+	osig.Notify(ch, os.Interrupt, syscall.SIGTERM)
+	<-ch
 
 	if cb != nil {
 		cb()
 	}
+}
+
+func WithSignal(parent context.Context) context.Context {
+	return WithSignalEx(parent, nil)
+}
+
+func WithSignalEx(parent context.Context, cb func()) context.Context {
+	ch := make(chan os.Signal)
+	osig.Notify(ch, os.Interrupt, syscall.SIGTERM)
+
+	ctx, cancel := context.WithCancel(parent)
+	go func() {
+		select {
+		case <-ch:
+			if cb != nil {
+				cb()
+			}
+			cancel()
+		case <-ctx.Done():
+		}
+	}()
+
+	return ctx
 }
